cfb425ab22eda13b0330acbb55bd868baa267d02
Vojtech Polasek <vpolasek@redhat.com>
2019-09-19 17:16:49 +0200

    creating intallation iso bootable on efi systems as well

--- prepare_iso.sh
+++ prepare_iso.sh
@@ -16,12 +16,16 @@ cp -a $mountdir/* $fedoradir
 echo "unmounting ISO..."
 fusermount -u $mountdir
 rmdir $mountdir
-echo "copying kickstart file..."
+echo "copying kickstart file into $fedoradir"
 cp ks.cfg $fedoradir
 echo "modifying isolinux.cfg..."
 sed -i '/menu default/d' $fedoradir/isolinux/isolinux.cfg
 sed -i '/label linux/i label kickstart\n  menu label \^Install accessible Fedora 30 Mate\n  menu default\n  kernel vmlinuz\n  append initrd=initrd\.img inst\.ks=hd:LABEL=fedora console=ttyS0,9600\n' $fedoradir/isolinux/isolinux.cfg
+echo "modifying EFI grub.cfg..."
+sed -i "s/^set default=\"1\"$/set default=\"0\"/" $fedoradir/EFI/BOOT/grub.cfg
+sed -i "/\#\#\# BEGIN \/etc\/grub\.d\/10\_linux \#\#\#/a menuentry \'Install accessible Fedora 30\' --class fedora --class gnu-linux --class gnu --class os \{\n\tlinuxefi \/images\/pxeboot\/vmlinuz inst\.ks=hd:LABEL=fedora console=ttyS0,9600\n\tinitrdefi \/images\/pxeboot\/initrd\.img\n\}\n" $fedoradir/EFI/BOOT/grub.cfg
+read -n 1 -s
 echo "repacking iso..."
-mkisofs -o fegora.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -J -R -V fedora $fedoradir
+mkisofs -o fegora.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -J -R -V fedora -eltorito-alt-boot -e images/efiboot.img -no-emul-boot $fedoradir
 echo "cleaning up..."
-rm -r $fedoradir
+rm -rf $fedoradir
