e962998ca72b5a2fcc53ffb8c73ff305f1f1bbdb
Vitezslav Humpa <vhumpa@redhat.com>
2025-05-13 15:37:06 +0200

    Remove qecore, add some helper functions and base-out the environment

--- tests/features/steps/steps.py
+++ tests/features/steps/steps.py
@@ -11,65 +11,74 @@ from dogtail.rawinput import (  # pylint: disable=import-error
     click,
     pressKey,
 )
-from qecore.utility import run, Tuple
-
-# Find a better solution how to get the precoded steps here instead of linter disable.
-from qecore.common_steps import *  # pylint: disable=unused-wildcard-import,wildcard-import
-
-from qecore.logger import Logging
 
 LOGGING = Logging()
 
+def hold_key(key_name):
+    code = keyNameToKeyCode(key_name)
+    registry.generateKeyboardEvent(code, None, KEY_PRESS)
+    doDelay(1)
+
+def release_key(key_name):
+    code = keyNameToKeyCode(key_name)
+    registry.generateKeyboardEvent(code, None, KEY_RELEASE)
+    doDelay(1)
+
+def run(context, command, *a, **kw):
+    try:
+        output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, *a, **kw).decode('utf8')
+        returncode = 0
+        exception = None
+    except subprocess.CalledProcessError as e:
+        output = e.output
+        returncode = e.returncode
+        exception = e
+    context.embed('text/plain', '$?=%d' % returncode, caption='%s result' % command)
+    context.embed('text/plain', output, caption='%s output' % command)
+    print(("%s output: %s" % (command,output)))
+    return output, returncode, exception
+
+def command_output(context, command, *a, **kw):
+    output, code, e = run(context, command, *a, **kw)
+    if code != 0:
+        raise e
+    return output
+
+def command_code(context, command, *a, **kw):
+    _, code, _ = run(context, command, *a, **kw)
+    return code
+
+def wait_until_showing(element):
+    wait_until(lambda x: x.position[0] > 0, element)
+    doDelay(0.25)
+
+def isProcessRunning(process):
+    '''Gives true if process can be greped out of full ps dump '''
+    s = subprocess.Popen(["ps", "axw"], stdout=subprocess.PIPE)
+    for x in s.stdout:
+        if re.search(process, x.decode('utf8')):
+            return True
+    return False
+
+@step(u'Press "{sequence}"')
+def press_button_sequence(context, sequence):  # pylint: disable=W0613
+    keyCombo(sequence)
+    sleep(0.5)
+
+
+@step('Execute "{command}" command')
+def execute_command(context, command):
+    os.system(command)
+
+
+@step('Wait for at least "{secs}" seconds')
+def wait_for_secs(context, secs):
+    sleep(int(secs))
+
 
 @step('Execute in terminal: "{command}"')
-def execute_command(context, command) -> None:  # pylint: disable=unused-argument
-    """
-    Execute command in terminal.
-
-    :param context: Holds contextual information during the running of tests.
-    :type context: <behave.runner.Context>
-
-    :param command: Command to execute.
-    :type command: str
-    """
-
+def exec_in_terminal(context, command) -> None:  # pylint: disable=unused-argument
     typeText(command)
     pressKey("Enter")
 
 
-@step("Window is fullscreen")
-@step("Window is {negation} fullscreen")
-def window_is_fullscreen(context, negation=None) -> None:
-    """
-    Verify that the terminal window is fullscreen.
-
-    :param context: Holds contextual information during the running of tests.
-    :type context: <behave.runner.Context>
-
-    :param negation: Negation of result when comparing, defaults to None.
-    :type negation: str, optional.
-    """
-
-    sleep(3)
-    frame = context.terminal.instance.child(roleName="frame")
-    size_x = frame.size[0]
-    size_y = frame.size[1]
-
-    screen_size_x = context.sandbox.resolution[0]
-    screen_size_y = context.sandbox.resolution[1]
-
-    if negation:
-        assert (
-            screen_size_x != size_x or screen_size_y != size_y
-        ), "Window is still in fullscreen mode."
-    else:
-        assert screen_size_x == size_x and screen_size_y == size_y, "\n".join(
-            (
-                "\nWindow is not in fullscreen mode.",
-                f"Existing resolution='{context.sandbox.resolution}'",
-                f"X: Screen Size='{screen_size_x}' != Terminal Size='{size_x}'.",
-                f"Y: Screen Size='{screen_size_y}' != Terminal Size='{size_y}'.",
-            )
-        )
-
-        assert frame.position[1] == 0, "Fullscreen frame is not top aligned."
